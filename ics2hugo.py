4745e5868f31d00f05a672b811c0087e6075f8e4
Yannic Haupenthal <yhaupenthal@posteo.de>
2024-02-18 15:58:57 +0100

    pylint and exit with proper code if sth goes wrong

--- ics2hugo.py
+++ ics2hugo.py
@@ -1,11 +1,16 @@
-from ics import Calendar
+"""Small script to create a markdown file with date, time and name of
+future events given by an URL containing an ics calendar"""
+
 import argparse
+import sys
+from ics import Calendar
 import arrow
 import requests
 #import re
 
-def write_hugo(path,events):
-    #for event in events:
+def write_hugo(path, entries):
+    """Write date, time and name to a markdown file"""
+    #for event in entries:
     #    fname = event.name
     #    fname = fname.replace(' ','-')
     #    fname = re.sub('[^0-9a-zA-Z-]*','',fname)
@@ -16,8 +21,8 @@ def write_hugo(path,events):
     #        mdfile.write('title = \"'+event.name+'\"\n')
     #        mdfile.write('+++\n\n')
     #        mdfile.write(str(event.description))
-    with open(path+'/events.md','w') as mdfile:
-        for event in events:
+    with open(path+'/events.md', mode='w', encoding='utf-8') as mdfile:
+        for event in entries:
             local = event.begin.to('Europe/Berlin')
             mdfile.write('* '+local.format('YYYY-MM-DD, HH:mm')+': '+event.name+'\n')
 
@@ -27,9 +32,10 @@ if __name__ == '__main__':
     parser.add_argument('--path', required=True ,help='output path of markdown files.')
     args = parser.parse_args()
     try:
-        ics = Calendar(requests.get(args.url).text)
+        ics = Calendar(requests.get(args.url, timeout=10).text)
         events = list(ics.timeline.start_after(arrow.now()))
         #events = list(ics.timeline)
-        write_hugo(args.path,events)
-    except:
-        print("An exception occurred")
+        write_hugo(args.path, events)
+    except Exception as e: # pylint: disable=broad-exception-caught
+        print(f"Error '{0}' occured. Arguments {1}.".format(e.message, e.args))
+        sys.exit(1)
